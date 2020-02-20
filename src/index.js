3019368e4b650643de91d6d90619cf68b4539abc
Ivan Kondzerau <konderev.ivan@gmail.com>
2020-02-20 12:46:51 +0100

    feat: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  var arr = String(n).split('');
+  if (arr[0] === '-') {
+      arr.shift();
+  }
+  var reverseStr = arr.reverse().join('');
+  return Number(reverseStr);
 }
